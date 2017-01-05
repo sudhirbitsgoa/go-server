2391df14948be8a7bfeea478b49f26a084c025b6
sudhir <sudhirbitsgoa@gmail.com>
2017-01-05 22:50:58 +0530

    adding cpu realtime graph in dashboard

--- client/monitIT/src/app/line-chart/line-chart.component.ts
+++ client/monitIT/src/app/line-chart/line-chart.component.ts
@@ -9,14 +9,16 @@ import {MetricServiceService} from '../metric-service.service'
 })
 export class LineChartComponent implements OnInit {
   private metricService: MetricServiceService;
+  private grapData: Array<Number>
+  private timeStamp: Array<any>
   constructor(metricService: MetricServiceService) {
       this.metricService = metricService;
   }
   // lineChart
   public lineChartData: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    { data: [], label: 'Series A' },
+    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    // { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: any = {
@@ -24,32 +26,16 @@ export class LineChartComponent implements OnInit {
     responsive: true
   };
   ngOnInit() {
+    this.grapData = []
+    this.timeStamp = []
     this.metricService.GetCPUMEMMetric(data => {
-        console.log('this is data', data);
+        this.grapData.push(data.perct);
+        let date = new Date(data.timeStamp);
+        this.timeStamp.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+        // this.lineChartData[0].data.push(data.perct);
+        this.lineChartData =  [{ data: this.grapData, label: data.metric }];
+        this.lineChartLabels = this.timeStamp;
     });
-    // (function(self) {
-    //   var conn;
-    //   // var msg = $("#msg");
-    //   // var log = $("#log");
-    //   if (window["WebSocket"]) {
-    //     conn = new WebSocket("ws://localhost:8080/ws");
-    //     conn.onopen = function () {
-    //         conn.send('cpu');
-    //     }
-    //     conn.onclose = function(evt) {
-    //       // appendLog($("<div><b>Connection closed.</b></div>"))
-    //     }
-    //     conn.onmessage = function(evt) {
-    //       console.log(evt.data)
-    //       let data = JSON.parse(evt.data);
-    //       self.lineChartData[0].data.push(data.perct);
-    //
-    //       // appendLog($("<pre/>").text(evt.data))
-    //     }
-    //   } else {
-    //     // appendLog($("<div><b>Your browser does not support WebSockets.</b></div>"))
-    //   }
-    // })(this);
   }
   public lineChartColors: Array<any> = [
     { // grey
@@ -79,17 +65,6 @@ export class LineChartComponent implements OnInit {
   ];
   public lineChartLegend: boolean = true;
   public lineChartType: string = 'line';
-
-  public randomize(): void {
-
-    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-  }
 
   // events
   public chartClicked(e: any): void {
--- client/monitIT/src/app/metric-service.service.ts
+++ client/monitIT/src/app/metric-service.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
-import {$WebSocket, WebSocketSendMode} from 'angular2-websocket/angular2-websocket'
+// import {$WebSocket, WebSocketSendMode} from 'angular2-websocket/angular2-websocket'
 
 @Injectable()
 export class MetricServiceService {
-  private ws: $WebSocket;
+  private ws: WebSocket;
   constructor() {
-      this.ws = new $WebSocket("ws://localhost:8080/ws");
+      this.ws = new WebSocket("ws://localhost:8080/ws");
   }
   GetCPUMEMMetric(cb) {
-    this.ws.onMessage((evt: MessageEvent) => {
-        cb(evt.data);
-    })
+    // this.ws.onMessage((evt: MessageEvent) => {
+    //     cb(evt.data);
+    // })
     // this.ws.onMessageHandler((evt: MessageEvent) => {
     //     console.log('whats wrong')
     // })
-    this.ws.send('cpu')//.subscribe(
+    // this.ws.send('cpu')//.subscribe(
     //     (msg)=> {
     //         console.log("next", msg.data);
     //         cb(msg.data);
@@ -25,5 +25,17 @@ export class MetricServiceService {
     //     ()=> {
     //         console.log("complete");
     //     })
+    // this.ws.send('cpu');
+    this.ws.onopen = () => {
+        this.ws.send('cpu');
+    }
+    this.ws.onclose = function(evt) {
+      // appendLog($("<div><b>Connection closed.</b></div>"))
+    }
+    this.ws.onmessage = function(evt) {
+      console.log(evt.data);
+      let data = JSON.parse(evt.data);
+      cb(data);
+    }
   }
 }
